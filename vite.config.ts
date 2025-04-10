5348ed024dfadd8c3731f5f4d2ddfe5de34bcf6f
Marc <mmissing3@gmail.com>
2025-04-09 23:53:21 -0700

    update dev server

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig(({ mode }) => {
       allowedHosts: [
         'localhost',
         'devserver-ui--promptcompare.netlify.app',
+        'devserver-Functionality--promptcompare.netlify.app',
         'devserver-businesslogic--promptcompare.netlify.app'
       ],
     },
